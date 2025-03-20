98cf7189d4cb7e11707e7ba39fcb74464c0848f2
윤종환 <yunjonghwan99@gmail.com>
2025-03-20 14:27:35 +0900

    입력한 데이터 router에서 값 확인

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const db = require('../db/database');
 
 router.get('/data/user', (req, res) => {
-  console.log('in router');
+  console.log(req.query.id);
+  console.log(req.query.name);
+  console.log(req.query.address);
 })
 
 module.exports = router;
