02088ef80d5681f9e18b6a27cfae9603af3c6368
최현준 <choijjunii1@gmail.com>
2025-03-20 14:48:20 +0900

    readme 생성

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import { db } from "./sqlprac/db/database.js";
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
+// cors(Cross-Origin Resource Sharing) 다른 도메인에서 오는 요청을 허용하기위해 필요함.
+import cors from "cors";
+// 클라이언트가 보낸 JSON 데이터를 서버에서 읽을 수 있도록 도와줌
+import bodyParser from "body-parser";
 
 dotenv.config();
 
-
-
-
-
-
-
+const app = express();
+// cors 미들웨어 설정
+app.use(cors());
+app.use(bodyParser.json());
 
 
 app.listen(process.env.PORT, () => {
